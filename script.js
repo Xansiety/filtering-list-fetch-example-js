638c580dc31e0289a2cf3a820fc7008484cf3686
xansiety <ferando543@outlook.com>
2022-10-09 18:11:53 -0500

    Ejemplo de Fetch y filtrado de lista

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('#searchInput')
+const userList = document.querySelector('#users')
+
+let users = []
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+  userList.innerHTML = '<h1>Cargando...</h1>'
+
+  const data = await loadUsers()
+  //console.log(data.data)
+  users = data.data
+  renderUsers(users)
+})
+
+
+async function loadUsers() {
+  const response = await fetch('https://fakerapi.it/api/v1/users?_quantity=1000')
+  const data = await response.json()
+  return data
+}
+
+
+input.addEventListener('keyup', event => {
+  const filteredUsers = users.filter(user => `${user.firstname.toLowerCase()} ${user.lastname.toLowerCase()}}`.includes(input.value.toLowerCase()))
+  renderUsers(filteredUsers)
+})
+
+const createUserItems = users => users.map(user => `<li class="bg-zinc-800 hover:bg-zinc-700 hover:cursor-pointer"> ${user.firstname} ${user.lastname} </li>`).join(' ')
+
+function renderUsers(users) {
+  const itemsString = createUserItems(users)
+  userList.innerHTML = itemsString
+}
